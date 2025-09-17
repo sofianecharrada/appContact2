3cac85d0fd2484f1e78006b6c8beb8addc62c839
sofianecharrada <sofianecharrada@gmail.com>
2025-09-17 22:34:05 +0200

    deploiement front end essai 5

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,7 +1,20 @@
 import cors from 'cors';
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://my-frontend.netlify.app"
+];
+
 const corsOptions = {
-  origin: 'http://localhost:3000', 
+  origin: function (origin, callback) {
+    // autorise les requêtes sans origine (ex: Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   optionsSuccessStatus: 200
 };
 
